a47eed4c8472fa5f137d9ab020938b45ca934046
moami <mo.amine.az@gmail.com>
2022-04-02 01:39:04 +0000

    templat done

--- app/src/main/java/com/example/tinderclone/Frament_Maneger/singup2frg.java
+++ app/src/main/java/com/example/tinderclone/Frament_Maneger/singup2frg.java
@@ -9,7 +9,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
 import com.example.tinderclone.R;
 
 public class singup2frg extends Fragment {
